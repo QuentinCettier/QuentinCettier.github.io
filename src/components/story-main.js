827e8b20057a86cb7d26f6296b3a7ff48058b39b
tristan-lanoye <tristanyjlanoye@gmail.com>
2017-12-19 13:17:18 +0100

    temp

--- src/components/story-main.js
+++ src/components/story-main.js
@@ -5,7 +5,7 @@ const flightDetails = document.querySelector('.flight_details')
 const fetchJSON = (url, index) => {
     fetch(url).then((res) => res.json()).then((data) => {
         console.log(data[index])
-        flightDetails.innerText = `${data[index].details}`
+        // flightDetails.innerText = `${data[index].details}`
     }).catch((err) => console.log('Data not found'))
 }
 
